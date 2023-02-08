4bae2fac6241f0a091bd40f17236de0207cd7231
MorganeBENUREAU <morgane.benureau@gmail.com>
2023-02-08 10:50:21 +0100

    reglage menu burger ecran tablette

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -35,54 +35,54 @@ const Header = () => {
 
         <ul
           id="toggled-menu"
-          className={`w-full absolute top-full left-0 -z-10 flex flex-col items-center lg:static lg:z-10 lg:w-max lg:transform-none lg:flex-row 
-            ${isActive ? "bg-gray-200 h-screen justify-center -mt-22 lg:h-auto lg:mt-0" : "-translate-y-full "}`}
+          className={`w-full absolute top-full left-0 -z-10 flex flex-col items-center justify-center lg:static lg:z-10 lg:w-max lg:transform-none lg:flex-row 
+            ${isActive ? "bg-gray-200 h-screen -mt-22 lg:h-auto lg:mt-0" : "-translate-y-full "}`}
           onClick={toggleNav}
         >
-          <li className="pb-6 md:py-0 md:mr-6">
+          <li className="pb-6 lg:py-0 lg:mr-6">
             <NavLink
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               to="/"
             >
               Accueil
             </NavLink>
           </li>
 
-          <li className="py-6 md:py-0 md:mr-6">
+          <li className="py-6 lg:py-0 lg:mr-6">
             <NavLink
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               to="/formations"
             >
               Formations
             </NavLink>
           </li>
-          <li className="py-6 md:py-0 md:mr-6">
+          <li className="py-6 lg:py-0 lg:mr-6">
             <NavLink
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               to="/experiences"
             >
               Expériences
             </NavLink>
           </li>
-          <li className="py-6 md:py-0 md:mr-6">
+          <li className="py-6 lg:py-0 lg:mr-6">
             <NavLink
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               to="/projets"
             >
               Projets
             </NavLink>
           </li>
-          <li className="py-6 md:py-0 md:mr-6">
+          <li className="py-6 lg:py-0 lg:mr-6">
             <NavLink
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               to="/contact"
             >
               Contact
             </NavLink>
           </li>
-          <li className="py-6 md:py-0 md:mr-6">
+          <li className="py-6 lg:py-0 lg:mr-6">
             <a
-              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 md:text-lg lg:text-xl"
+              className="text-2xl uppercase font-semibold w-full hover:underline underline-offset-4 lg:text-xl"
               href="/CV.pdf" target="_blank" rel="noopener noreferrer"
             >
               CV
--- src/components/Projets/index.js
+++ src/components/Projets/index.js
@@ -1,5 +1,5 @@
 import node from '../../images/logo-nodeJS.png';
-import koa from '../../images/logo-koa.png';
+// import koa from '../../images/logo-koa.png';
 import postgresql from '../../images/logo-postgresql.png';
 import react from '../../images/logo-react.png';
 import tailwind from '../../images/logo-tailwindcss.png';
@@ -7,23 +7,28 @@ import tailwind from '../../images/logo-tailwindcss.png';
 
 function Projets() {
   return (
-    <main className="font-quicksand flex flex-col justify-around items-center text-left indent-4 flex-1-0-auto w-4/5 mt-28 mb-2 mx-auto sm:mt-20 sm:w-4/5 md:w-3/5 lg:w-2/5">
+    <main className="font-quicksand flex flex-col justify-around items-center text-left flex-1-0-auto w-4/5 mt-28 mb-2 mx-auto sm:mt-20 sm:w-4/5 md:w-3/5 lg:w-2/5">
       <h1 className="font-cabin text-2xl uppercase mb-4">Projets</h1>
       <section className="p-2 shadow shadow-custom-blue">
         <h2 className="font-cabin text-xl mb-2 text-center">NotaBebe</h2>
-        <div>
-          <img src="" alt="" />
+        <div className="flex w-full justify-around items-center my-2">
+          <img src={node} alt="" className="h-10 md:h-12"/>
+          <img src={postgresql} alt="" className="h-10 md:h-12"/>
+          <img src={react} alt="" className="h-10 md:h-12"/>
         </div>
+        <p className="indent-4">
+          Projet réalisé en groupe de 5 (3 personnes côté front, 2 côté back, dont moi-même). Ce projet a été fait dans le cadre de ma formation O'clock comme projet de fin d'études. C'est un site qui permet de faire le lien entre des parents et une structure d'accueil comme une crèche. 
+        </p>
         <p>
-          Projet réalisé en groupe de 5 (3 personnes côté front, 2 côté back, dont moi-même). Ce projet a été fait dans le cadre de ma formation O'clock comme projet de fin d'études. C'est un site qui permet de faire le lien entre des parents et une structure d'accueil comme une crèche. <a href="https://notabebe.netlify.app/">Lien du projet.</a>
+          <a className="hover:underline" href="https://notabebe.netlify.app/">Lien du projet.</a>
         </p>
       </section>
       <section className="mt-6 p-2 shadow shadow-custom-blue">
         <h2 className="font-cabin text-xl mb-2 text-center">Bibli'O</h2>
-        <div>
-          <img src="" alt="" />
+        <div className="flex w-full justify-around items-center my-2">
+          <img src={node} alt="" className="h-10 md:h-12"/>
+          <img src={postgresql} alt="" className="h-10 md:h-12"/>
         </div>
-         nodeJSpostgresQL 
         <p>
           Api de bibliothèque personnelle faite en NodeJS
           et PostgreSQL. Début de front en EJS, mon but est de faire le front en
@@ -44,15 +49,17 @@ function Projets() {
       </section> */}
       <section className="mt-6 p-2 shadow shadow-custom-blue">
         <h2 className="font-cabin text-xl mb-2 text-center">Martine Angibaud</h2>
-        <div>
-          <img src="" alt="" />
+        <div className="flex w-full justify-around items-center my-2">
+          <img src={react} alt="" className="h-10 md:h-12"/>
         </div>
         <p>
           Création site vitrine pour particulier react Apprentissage de leaflet
           pour ajouter une carte et firebase pour héberger le site. Lien du site.
         </p>
       </section>
-      <section className="mt-6 p-2 shadow shadow-custom-blue">Projet à venir</section>
+      <section className="w-full mt-6 p-2 shadow shadow-custom-blue">
+        <h2 className="font-cabin text-xl mb-2 text-center">Projet à venir</h2>
+        </section>
     </main>
   );
 }
